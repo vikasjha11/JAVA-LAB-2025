56727b98ddd73483e624ecfa58a35f81479c969f
Vikas Kumar Jha <vikaskumarjha763@gmail.com>
2025-02-15 11:34:37 +0530

    Add files via upload

---
+++ Week 1/Hellorun.java
@@ -0,0 +1,6 @@
+public class Hellorun
+{
+public static void main(string[] args)
+{
+System.out.println("Hello World");
+}}
---
+++ Week 1/cmdline.java
@@ -0,0 +1,14 @@
+public class cmdline
+{
+    public static void main(String[] args)
+    {
+        String name=args[0];
+        String course=args[1];
+        String university=args[2];
+        String sem=args[3];
+        System.out.println("Name:"+name);
+        System.out.println("Course:"+course);
+        System.out.println("University"+university);
+        System.out.println("Sem:"+sem);
+    }
+}
